5904dc24a4e6521226cff160b3a8f5f6a0e63f56
joaodick <joaodick@v.cluster.local>
2024-02-14 16:10:34 -0500

    New metric

--- sbatch_exp.sh
+++ sbatch_exp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PM=$1
+ZDIM=$1
 
 
 sbatch <<EOF
@@ -9,27 +9,26 @@ sbatch <<EOF
 #SBATCH -c 10
 #SBATCH --mem=40GB
 #SBATCH --time=8:00:00
-#SBATCH --job-name=PM_$PM
+#SBATCH --job-name=$ZDIM
 #SBATCH --output=exp.log
 #SBATCH --qos=m2
-#SBATCH --exclude=gpu096
+#SBATCH --exclude=gpu096,gpu106
 # activate environment
 module load pytorch1.13-cuda11.7-python3.7
 
 
-
-python train-R1eps.py --total_epochs 120 \
+python train-R1eps-NEW.py --total_epochs 120 \
 				--lambda_P 0 \
-				--lambda_PM $PM \
+				--lambda_PN 0 \
 				--lambda_MSE 1 \
 				--lambda_gp 10 \
 				--bs 64 \
-				--skip_fq 10 \
+				--skip_fq 1 \
 				--dim 128 \
-				--z_dim 4 \
+				--z_dim $ZDIM \
 				--L 2 \
 				--d_penalty 0 \
-				--pre_path './saved_models/R1-eps|_dim_128|z_dim_4|L_2|lambda_gp_10|bs_64|dpenalty_0.0|lambdaP_0.0|lambdaPM_0.0|lambdaMSE_1.0' \
+				--pre_path 'None' \
 				--path './data/' 
 
 #./saved_models/R1-eps|_dim_128|z_dim_4|L_2|lambda_gp_10|bs_64|dpenalty_0.0|lambdaP_0.0|lambdaPM_0.0|lambdaMSE_1.0' \
