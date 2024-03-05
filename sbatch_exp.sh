fb34e452183ce973c5bbebd5d3f4032da2dccb9f
joaodick <joaodick@v3.cluster.local>
2024-03-04 20:48:39 -0500

    R1-eps updates

--- sbatch_exp.sh
+++ sbatch_exp.sh
@@ -16,20 +16,23 @@ module load pytorch2.0-cuda11.8-python3.9
 #pytorch1.13-cuda11.7-python3.7
 
 
-python train-R1eps-NEW.py --total_epochs 100 \
-				--lambda_P $1 \
+python train-R1eps-NEW.py --total_epochs 80 \
+				--lambda_P 0 \
 				--lambda_MSE 1 \
 				--lambda_PN 0 \
+				--lambda_PM $1 \
 				--lambda_gp 10 \
 				--bs 64 \
 				--skip_fq 5 \
 				--dim 128 \
-				--z_dim 12\
+				--z_dim 6\
 				--L 2 \
 				--d_penalty 0 \
-				--pre_path "./saved_models/R1-eps|_dim_128|z_dim_12|L_2|lambda_gp_10|bs_64|dpenalty_0.0|lambdaP_$2|lambdaPN_0.0|lambdaMSE_1.0" \
+				--pre_path './saved_models/12/R1-eps|dim_128|z_dim_6|L_2|lambda_gp_10|bs_64|dpenalty_0.0|lambdaP_0.0|lambdaPM_$2|lambdaPN_0.0|lambdaMSE_1.0'\
 				--path './data/' 
 
 #'./saved_models/R1-eps|_dim_128|z_dim_4|L_2|lambda_gp_10|bs_64|dpenalty_0.0|lambdaP_0.0|lambdaPM_0.0|lambdaMSE_1.0' \
 #'./saved_models/R1-eps|_dim_128|z_dim_12|L_2|lambda_gp_10|bs_64|dpenalty_0.0|lambdaP_0.0|lambdaPN_0.0|lambdaMSE_1.0'
+#'R1-eps|dim_128|z_dim_1|L_2|lambda_gp_10|bs_64|dpenalty_0.0|lambdaP_0.0|lambdaPM_0.0|lambdaPN_0.0|lambdaMSE_1.0'
+
 EOF
